70dac88bcaaa16e07efdd43049a182295edfdaec
David Stojanovski <David.Stojanovski@netcetera.com>
2018-11-25 13:47:28 +0100

    Changed marged from 2 branches

--- src/components/Heroes.js
+++ src/components/Heroes.js
@@ -3,27 +3,11 @@ import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {fetchHeroes} from '../actions/heroActions';
 import {fetchMatches} from "../actions/matchesAction";
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import { Link } from 'react-router-dom'
 import GamingBuddyCard from "../Card/GamingBuddyCard";
 
 class Heroes extends Component {
     componentWillMount() {
         this.props.fetchHeroes();
-        this.props.fetchMatches();
-    }
-
-    calculateHeroHealth(baseStr){
-        return parseInt(baseStr) * 18 + 200;
-    }
-
-    calculateHeroMana(baseInt) {
-        return parseInt(baseInt) * 12 + 75;
     }
 
     render() {
@@ -41,14 +25,12 @@ class Heroes extends Component {
 
 Heroes.propTypes = {
     fetchHeroes: PropTypes.func.isRequired,
-    fetchMatches: PropTypes.func.isRequired,
     heroes: PropTypes.array,
 
 };
 
 const mapStateToProps = state => ({
     heroes: state.heroes,
-    matches: state.matches,
 });
 
-export default connect(mapStateToProps, {fetchHeroes, fetchMatches})(Heroes);
+export default connect(mapStateToProps, {fetchHeroes})(Heroes);
